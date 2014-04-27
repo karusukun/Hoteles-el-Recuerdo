c9d909187a15071834a3a28056e103aa5dbf329b
boko <karusukun@gmail.com>
2014-04-26 18:41:03 -0600

    Creacion del MV
    inicialiacion del projecto

---
+++ Hoteles del Recuerdo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hoteles_del_Recuerdo.MvcApplication" Language="C#" %>
---
+++ Hoteles del Recuerdo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hoteles_del_Recuerdo.Startup))]
+namespace Hoteles_del_Recuerdo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
